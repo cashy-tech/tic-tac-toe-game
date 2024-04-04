086c48e0f17d2a25273a4067138da51157385d52
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-04 12:43:18 +0300

    First commit

---
+++ src/square.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Square = () => {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
